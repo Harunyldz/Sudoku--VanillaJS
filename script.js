f88ccacc714bc42a02296906494e6a1813c39a47
Harun Yıldız <67275880+Harunyldz@users.noreply.github.com>
2024-03-24 00:37:18 +0300

    undo delete and hint operations created

--- script.js
+++ script.js
@@ -3,17 +3,23 @@ const numbers = document.querySelectorAll(".number")
 const pausePlayImg = document.getElementById("pausePlay")
 const playIcon = document.getElementById("playIcon")
 const mistakeSpan = document.getElementById("mistakeCount")
-const loseModal=document.querySelector(".loseModal")
-const overlay=document.getElementById("overlay")
-const newGameModal=document.querySelector(".newGameModal")
-const newGameBtnModal=document.getElementById("newGameBtnModal")
-const modalCancelBtn=document.getElementById("modalCancelBtn")
-const modalRestartBtn=document.getElementById("modalRestartBtn")
-const modalLevels=document.querySelectorAll(".modalLevelItem")
+const loseModal = document.querySelector(".loseModal")
+const overlay = document.getElementById("overlay")
+const newGameModal = document.querySelector(".newGameModal")
+const newGameBtnModal = document.getElementById("newGameBtnModal")
+const modalCancelBtn = document.getElementById("modalCancelBtn")
+const modalRestartBtn = document.getElementById("modalRestartBtn")
+const modalLevels = document.querySelectorAll(".modalLevelItem")
+const hintDiv = document.getElementById("hintDiv")
+const hintCountSpan = document.getElementById("hintCount")
+const undoDiv = document.getElementById("undo")
+const deleteDiv = document.getElementById("delete")
+const selectLevels = document.getElementById("levels")
+const pausePlayDiv = document.getElementById("pause")
+const newGameBtn = document.getElementById("newGameBtn")
 
-let isGameStarted=true
+let isGameStarted = true
 let selectedNumber = ""
-let isCellSelected = false
 let selectedCellIndex = ""
 let openedCellCount = 40
 let selectedLevel = "easy"
@@ -25,72 +31,89 @@ let time = 0
 let isPaused = false
 let gameTimer
 let mistakeCount = 3
+let hintCount = 3
+let getUnOpenedCellsArray = []
+let lastClickedCellsIndexes = []
 
 import levels from "./utils/levels.js"
 
 getNewGame()
-console.log(openedCellIndexes)
 
 cells.forEach((cell, index) => {
     cell.addEventListener("click", () => {
-        selectedCellIndex = index
-        console.log(selectedCellIndex, ".cell clicked")
-        cells.forEach(cell=>{
-            cell.style.backgroundColor="#fff"
-        })
-        if(cell.textContent){
-            const getTextContent=cell.textContent
-            cells.forEach(cell=>{
-                // tıklanan hücreyle aynı değere sahip diğer hücreleri işaretle
-                cell.style.backgroundColor=cell.textContent===getTextContent? "#d2dce9":"#fff"
-            })
-        }else{
-            cell.style.backgroundColor="#d2dce9"
+        if (cell.getAttribute("data-isOpen") !== "opened") { //Başlangıçta açık olan hücreler değiştirilemesin
+            selectedCellIndex = index
+        } else {
+            selectedCellIndex = ""
         }
-        isCellSelected = true
+        console.log(selectedCellIndex, ".cell clicked")
+        cells.forEach(cell => {
+            cell.style.backgroundColor = "#fff"
+        })
+        if (cell.textContent) {
+            const getTextContent = cell.textContent
+            cells.forEach(cell => {
+                // tıklanan hücreyle aynı değere sahip diğer hücreleri işaretle
+                cell.style.backgroundColor = cell.textContent === getTextContent ? "#d2dce9" : "#fff"
+            })
+        } else {
+            cell.style.backgroundColor = "#d2dce9"
+        }
     })
 })
 
 numbers.forEach(number => {
     const cells = document.querySelectorAll(".cell")
-   
+
     number.addEventListener("click", () => {
-        if(isGameStarted){
+        if (isGameStarted) {
 
             selectedNumber = number.getAttribute("data-number-value")
             console.log(selectedNumber, " number clicked")
             const currentCell = cells[selectedCellIndex]
-            if (isCellSelected) {
-                const getCellValue = currentCell.getAttribute("data-cell-value")
+            if (selectedCellIndex) {
+                const getCellValue = currentCell.getAttribute("data-cell-value")//seçilen hücrede olması gereken değeri getirdi
+                lastClickedCellsIndexes.push({ cellindex: selectedCellIndex, value: currentCell.textContent })//geri al işlemi için tıklanmış hücrenin bir önceki textcontent değerini ve index değerini diziye ekledi
+                console.log("son tıklananlar: ", lastClickedCellsIndexes)
                 currentCell.style.color = getCellValue === selectedNumber ? "#325aaf" : "red"
-                currentCell.textContent = selectedNumber
-                cells.forEach(cell=>{
+                console.log("selcted ındex value: ", cells[selectedCellIndex].textContent)
+                if (cells[selectedCellIndex].textContent === selectedNumber) {//seçilmiş hücrenin değeri girilen sayıya eşitse
+                    cells[selectedCellIndex].textContent = ""
+                    currentCell.setAttribute("data-selected-value", "")
+                } else {
+                    currentCell.textContent = selectedNumber
+                    mistakeCount = getCellValue === selectedNumber ? mistakeCount : mistakeCount += 1
+                    currentCell.setAttribute("data-selected-value", selectedNumber)
+                }
+                console.log("selcted ındex value cahged: ", cells[selectedCellIndex].textContent)
+                console.log("selctedındex: ", selectedCellIndex)
+                // if(cells[selectedCellIndex])
+                cells.forEach(cell => {
                     //hücreye girilen değerle aynı değere sahip diğer hücreleri işaretle
-                    cell.style.backgroundColor=cell.textContent===currentCell.textContent? "#d2dce9":"#fff"
+                    if (cell.textContent)
+                        cell.style.backgroundColor = cell.textContent === currentCell.textContent ? "#d2dce9" : "#fff"
                 })
-                mistakeCount = getCellValue === selectedNumber ? mistakeCount : mistakeCount += 1
-                if(mistakeCount===3){
-                    loseModal.style.display="flex"
-                    overlay.style.display="block"
+                if (mistakeCount === 3) {
+                    loseModal.style.display = "flex"
+                    overlay.style.display = "block"
                     clearInterval(gameTimer)
-                    isGameStarted=false
+                    isGameStarted = false
                 }
                 displayMistakes(mistakeCount)
-                // geri al tuşuna tıklandığında seçilmiş olan değeri kullanabilmek için 
-                currentCell.setAttribute("data-selected-value", selectedNumber)
-        }
+                // play ikonuna tıklandığında seçilmiş olan değeri gösterebilmek için 
+            }
         }
     })
 })
 
-const selectLevels = document.getElementById("levels")
+//*****************************************************************Event Listeners*********************************************************
+
 selectLevels.addEventListener("change", () => {
     selectedLevel = selectLevels.value
     setLevel()
     getNewGame()
 })
 
-const pausePlayDiv = document.getElementById("pause")
 pausePlayDiv.addEventListener("click", () => {
     if (!isPaused) {
         pauseGame()
@@ -99,39 +122,80 @@ pausePlayDiv.addEventListener("click", () => {
     }
 })
 
-const newGameBtn = document.getElementById("newGameBtn")
 newGameBtn.addEventListener("click", () => {
     getNewGame()
 })
 
-newGameBtnModal.addEventListener("click",()=>{
-    loseModal.style.display="none"
-    newGameModal.style.display="block"
+newGameBtnModal.addEventListener("click", () => {
+    loseModal.style.display = "none"
+    newGameModal.style.display = "block"
 })
 
-modalCancelBtn.addEventListener("click",()=>{
-    newGameModal.style.display="none"
-    overlay.style.display="none"
+modalCancelBtn.addEventListener("click", () => {
+    closeModal()
 })
 
-modalRestartBtn.addEventListener("click",()=>{
-    overlay.style.display="none"
-    newGameModal.style.display="none"
+modalRestartBtn.addEventListener("click", () => {
+    closeModal()
     getNewGame()
 })
 
-modalLevels.forEach(modalLevel=>{
-    modalLevel.addEventListener("click",()=>{
-        let selectedModalLevel=modalLevel.getAttribute("data-level-value")
-        newGameModal.style.display="none"
-        overlay.style.display="none"
-        selectLevels.value=selectedModalLevel
-        selectedLevel = selectLevels.value
+modalLevels.forEach(modalLevel => {
+    modalLevel.addEventListener("click", () => {
+        let selectedModalLevel = modalLevel.getAttribute("data-level-value")//modal içindeki seçeneklerden data-level-value değeri alındı
+        newGameModal.style.display = "none"
+        overlay.style.display = "none"
+        selectLevels.value = selectedModalLevel//Select elementinin value bilgisi güncellendi
+        selectedLevel = selectedModalLevel
         setLevel()
         getNewGame()
     })
 })
 
+undoDiv.addEventListener("click", () => {
+    if (lastClickedCellsIndexes.length > 0) {//lastClickedCellsIndexes dizisinde eleman varsa
+        cells.forEach(cell => {
+            cell.style.backgroundColor = "#fff"
+        })
+        const lastClickedCell = lastClickedCellsIndexes[lastClickedCellsIndexes.length - 1].cellindex//lastClickedCellsIndexes dizisindeki son elemanın indis değerini aldı
+        const lastClickedCellValue = lastClickedCellsIndexes[lastClickedCellsIndexes.length - 1].value//lastClickedCellsIndexes dizisindeki son elemanın value değerini aldı
+        cells[lastClickedCell].textContent = lastClickedCellValue
+        cells[lastClickedCell].style.color = cells[lastClickedCell].getAttribute("data-cell-value") === lastClickedCellValue ? "#325aaf" : "red"//geri getirilen değerin doğruluğuna göre rengi belirlendi
+        cells[lastClickedCell].setAttribute("data-selected-value", lastClickedCellValue) // play ikonuna tıklandığında seçilmiş olan değeri gösterebilmek için 
+        lastClickedCellsIndexes.pop()
+        if (lastClickedCellsIndexes.length > 0) {
+            //bir önceki hamle yapılan hücrenin hangisi olduğunu belirtmek için
+            cells[lastClickedCellsIndexes[lastClickedCellsIndexes.length - 1].cellindex].style.backgroundColor = "#d2dce9"
+        }
+    }
+})
+
+deleteDiv.addEventListener("click", () => {
+    if (selectedCellIndex) {
+        //geri al işlemini sağlamak için var olan değer diziye eklenip sonra text content temizlenecek
+        lastClickedCellsIndexes.push({ cellindex: selectedCellIndex, value: cells[selectedCellIndex].textContent })
+        cells[selectedCellIndex].textContent = ""
+        cells[selectedCellIndex].setAttribute("data-selected-value", "")// play ikonuna tıklandığında seçilmiş olan değeri gösterebilmek için
+    }
+})
+
+hintDiv.addEventListener("click", () => {
+    if (isGameStarted) {
+        if (hintCount !== 0) {
+            hintCount -= 1
+            const hintCell = cells[getOneUnOpenedCell()]
+            hintCell.textContent = hintCell.getAttribute("data-cell-value")
+            hintCell.setAttribute("data-isOpen", "opened")
+            hintCell.classList.add("hintCell")
+            displayHintCounts()
+            console.log("unopenedcells: ", getUnOpenedCellsArray)
+        }
+        getUnOpenedCellsArray.length = 0//açılmamış hücreleri gösteren diziyi sıfırla
+    }
+})
+
+
+//*****************************************************************Functions*********************************************************
 //sudoku tahtasındaki rakamların hazırlanışı için gerekli fonksiyonlar
 function rowSafe(board, row, num) {
     return !board[row].includes(num);
@@ -250,7 +314,7 @@ function showOpenedCells() {
     const cells = document.querySelectorAll(".cell")
     openedCellIndexes.forEach(openedCell => {
         cells[openedCell].textContent = cells[openedCell].getAttribute("data-cell-value")
-        cells[openedCell].setAttribute("data-isOpen", "opened")
+        cells[openedCell].setAttribute("data-isOpen", "opened") //Başlangıçta açık olan hücreleri kontrol edebilmek için
     })
 }
 
@@ -264,9 +328,7 @@ function Timer() {
         minute = 0
         hour += 1
     }
-    displaySecond()
-    displayMinute()
-    displayHour()
+    displayTime()
 }
 
 function startTimer() {
@@ -278,22 +340,15 @@ function clearTimer() {
     second = 0
     minute = 0
     hour = 0
-    displaySecond()
-    displayMinute()
-    displayHour()
+    displayTime()
 }
 
-function displaySecond() {
+function displayTime() {
     const secondDiv = document.getElementById("second")
-    secondDiv.textContent = second > 9 ? second : "0" + second
-}
-function displayMinute() {
-    const minuteDiv = document.getElementById("minute")
-    minuteDiv.textContent = minute > 9 ? minute + ":" : "0" + minute + ":"
-}
-
-function displayHour() {
     const hourDiv = document.getElementById("hour")
+    const minuteDiv = document.getElementById("minute")
+    secondDiv.textContent = second > 9 ? second : "0" + second
+    minuteDiv.textContent = minute > 9 ? minute + ":" : "0" + minute + ":"
     hourDiv.style.visibility = hour > 0 ? "visible" : "hidden"
     hourDiv.textContent = hour > 9 ? hour + ":" : "0" + hour + ":"
 }
@@ -302,6 +357,7 @@ function pauseGame() {
     const cells = document.querySelectorAll(".cell")
     cells.forEach(cell => {
         cell.textContent = ""
+        cell.style.backgroundColor = "#fff"
     })
     pausePlayImg.src = "./assets/play2.svg"
     isPaused = true
@@ -312,6 +368,8 @@ function pauseGame() {
 function continueGame() {
     const cells = document.querySelectorAll(".cell")
     playIcon.style.display = "none"
+
+    //pauseGame fonk. çalıştığında textcontentler silindiğinden tekrar gösterebilmek için
     cells.forEach(cell => {
         if (cell.getAttribute("data-isOpen")) {
             cell.textContent = cell.getAttribute("data-cell-value")
@@ -329,13 +387,16 @@ function clearGame() {
     cells.forEach(cell => {
         cell.textContent = ""
         cell.style.color = "#265073"
-        cell.style.backgroundColor="#fff"
+        cell.style.backgroundColor = "#fff"
         cell.setAttribute("data-cell-value", "")
         cell.setAttribute("data-isOpen", "")
         cell.setAttribute("data-selected-value", "")
+        cell.classList.remove("hintCell")
     })
     clearTimer()
-    mistakeCount=0
+    mistakeCount = 0
+    hintCount = 3
+    lastClickedCellsIndexes.length = 0 //son tıklananlar dizisini sıfırla
 }
 
 function getNewGame() {
@@ -345,10 +406,30 @@ function getNewGame() {
     getOpenedCellsIndexes()
     showOpenedCells()
     startTimer()
-    displayMistakes(mistakeCount)
-    isGameStarted=true
+    displayMistakes()
+    displayHintCounts()
+    isGameStarted = true
 }
 
-function displayMistakes(mistakeCount) {
+function closeModal() {
+    newGameModal.style.display = "none"
+    overlay.style.display = "none"
+}
+
+function displayMistakes() {
     mistakeSpan.textContent = mistakeCount
+}
+
+function displayHintCounts() {
+    hintCountSpan.textContent = hintCount
+}
+
+function getOneUnOpenedCell() {
+    cells.forEach((cell, index) => {
+        if (cell.textContent === "") {//textcontent i olmayan hücrelerinin indislerini getUnOpenedCellsArray dizisine ekle
+            getUnOpenedCellsArray.push(index)
+        }
+    })
+    const shuffledUnOpenedCellsArray = shuffle(getUnOpenedCellsArray) //getUnOpenedCellsArray dizisini shuffle fonk. ile karıştır ve geri shuffledUnOpenedCellsArray dizisine ata
+    return shuffledUnOpenedCellsArray[0]  //shuffledUnOpenedCellsArray dizisinin ilk elemanını seç ve
 }
